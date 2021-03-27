72fc4bd0510f5d85255441f796bd62b87b9435c9
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2021-03-27 15:09:58 -0700

    Add portmod alias.

--- alias.zsh
+++ alias.zsh
@@ -8,6 +8,7 @@ fi
 alias l='ls -l'
 alias la='ls -la'
 alias ll=l
+alias omw='portmod openmw'
 
 alias pacman-list-by-size='pacman -Qi | awk '\''/^Name/ {name=$3} /^Installed Size/ {size=$4; size_hint=$5;printf "%4.0f%-3s    %s\n", size, size_hint, name}'\'' | sort -h -r'
 alias pacman-rm-orphans='sudo pacman -Rns $(pacman -Qtdq)'

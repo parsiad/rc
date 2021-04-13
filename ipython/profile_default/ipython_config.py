d5bb861e060edbd290b63e9d8c43b0c32dd8cda4
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2021-04-13 11:43:11 -0700

    Improve default IPython imports.

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,6 +1,9 @@
+c.InteractiveShellApp.exec_lines = [
+    'from __future__ import division, print_function',
+    'import numpy as np',
+    'import matplotlib.pyplot as plt',
+]
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
-
-import numpy as np

aa12cd3429ae7fd5fd04270774c5e125f534bf63
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2021-10-26 13:59:28 -0700

    Use hostname command for prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,2 @@
 setopt PROMPT_SUBST
-PROMPT='%F{4}[%F{3}${HOSTNAME}%F{4}] %F{2}$(sed "s:\([^/]\)[^/]*/:\1/:g" <<<$PWD)%F{4}> %f'
+PROMPT='%F{4}[%F{3}$(hostname)%F{4}] %F{2}$(sed "s:\([^/]\)[^/]*/:\1/:g" <<<$PWD)%F{4}> %f'

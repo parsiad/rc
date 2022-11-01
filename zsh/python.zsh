7c6d1218c08dc27c98e02d2bb2c5b3a6c7e0e59d
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2022-11-01 13:23:51 -0400

    Simplify Python zshrc.

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -1,7 +1,1 @@
-pythonpath_prepend() {
-    if [ -d "$1" ] && [[ ":$PYTHONPATH:" != *":$1:"* ]]; then
-        export PYTHONPATH="$1${PYTHONPATH:+":$PYTHONPATH"}"
-    fi
-}
-
-pythonpath_prepend ~/python
+export PYTHONPATH=$HOME/python

a93393a08735704b6f6ae89d185fd6bc6ca78de1
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2022-11-05 17:58:43 -0400

    Waste less space on themes and icons.

---
+++ icons.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -z ${PARSIAD_IS_AT_WORK} ]; then
+    exit 0
+fi
+
+mkdir -p icons
+cd icons
+
+wget -nc -O rtl88-Themes-1.0.zip \
+    https://github.com/rtlewis88/rtl88-Themes/archive/refs/tags/1.0.zip
+unzip -o rtl88-Themes-1.0.zip rtl88-Themes-1.0/Solarized-Colors-gtk-theme-iconpack_1.0.tar.gz
+cd rtl88-Themes-1.0
+tar xvfz Solarized-Colors-gtk-theme-iconpack_1.0.tar.gz Solarized-Blue-gtk-theme-iconpack
--- themes.sh
+++ themes.sh
@@ -4,12 +4,10 @@ if [ ! -z ${PARSIAD_IS_AT_WORK} ]; then
     exit 0
 fi
 
+mkdir -p themes
+cd themes
+
 wget -nc -O NumixSolarized-20210831.tar.gz \
     https://github.com/Ferdi265/numix-solarized-gtk-theme/releases/download/20210831/NumixSolarized-20210831.tar.gz
-tar xvfz NumixSolarized-20210831.tar.gz
+tar xvfz NumixSolarized-20210831.tar.gz NumixSolarized/NumixSolarizedDarkBlue
 
-wget -nc -O rtl88-Themes-1.0.zip \
-    https://github.com/rtlewis88/rtl88-Themes/archive/refs/tags/1.0.zip
-unzip rtl88-Themes-1.0.zip
-cd rtl88-Themes-1.0
-tar xvfz Solarized-Colors-gtk-theme-iconpack_1.0.tar.gz

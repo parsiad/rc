dc115dfdf83e2e829ec957c5f7410f90c016045a
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2022-12-25 14:55:17 -0500

    Add GlazeWM and refactor

--- themes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p themes
-cd themes
-
-wget -nc -O NumixSolarized-20210831.tar.gz \
-    https://github.com/Ferdi265/numix-solarized-gtk-theme/releases/download/20210831/NumixSolarized-20210831.tar.gz
-tar xvfz NumixSolarized-20210831.tar.gz NumixSolarized/NumixSolarizedDarkBlue
-

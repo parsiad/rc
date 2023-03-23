b6618702405d5e625efcd4f176eae29f0e460a5d
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2023-03-23 05:44:24 -0400

    Clean up IPython config

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,8 +1,1 @@
-c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-    # 'import matplotlib.pyplot as plt',
-]
 c.TerminalInteractiveShell.editing_mode = 'vi'
-
-# import matplotlib.pyplot as plt
-# plt.style.use('fivethirtyeight')

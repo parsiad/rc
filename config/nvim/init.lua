59b18882399c33c532c5ee0cf01786341aad7fbf
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2023-04-04 05:33:25 -0400

    Change init.lua to be compatible with nvim 0.5.1

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,9 @@
+vim.cmd([[
+autocmd vimenter * ++nested colorscheme solarized8
+execute "set t_8f=\e[38;2;%lu;%lu;%lum"
+execute "set t_8b=\e[48;2;%lu;%lu;%lum"
+]])
+
 local Plug = vim.fn["plug#"]
 vim.call("plug#begin")
 Plug("LnL7/vim-nix") -- Vim configuration files for Nix http://nixos.org/nix
@@ -16,10 +22,10 @@ Plug("junegunn/vim-peekaboo") -- " / @ / CTRL-R
 Plug("lifepillar/vim-solarized8") -- Optimized Solarized colorschemes. Best served with true-color terminals!
 Plug("mboughaba/i3config.vim") -- Vim syntax highlighting for i3 config
 Plug("ncm2/float-preview.nvim") -- Less annoying completion preview window based on neovim's floating window
-Plug("neovim/nvim-lspconfig") -- Quickstart configs for Nvim LSP
+Plug("neovim/nvim-lspconfig", { tag = "v0.1.0" }) -- Quickstart configs for Nvim LSP
 Plug("preservim/nerdtree") -- A tree explorer plugin for vim
 Plug("preservim/tagbar") -- Vim plugin that displays tags in a window, ordered by scope
-Plug("ray-x/lsp_signature.nvim") -- LSP signature hint as you type
+Plug("ray-x/lsp_signature.nvim", { tag = "v0.1.1" }) -- LSP signature hint as you type
 Plug("rcarriga/nvim-notify") -- A fancy, configurable, notification manager for NeoVim
 Plug("rust-lang/rust.vim") -- Vim configuration for Rust.
 Plug("ryanoasis/vim-devicons") -- Adds file type icons to Vim plugins such as: NERDTree, vim-airline, CtrlP, unite, Denite, lightline, vim-startify and many more
@@ -31,24 +37,12 @@ Plug("vim-airline/vim-airline-themes") -- A collection of themes for vim-airline
 Plug("vim-autoformat/vim-autoformat") -- Provide easy code formatting in Vim by integrating existing code formatters.
 vim.call("plug#end")
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, { command = "NERDTree" })
-vim.api.nvim_create_autocmd({ "VimEnter" }, { command = "wincmd p" })
-vim.api.nvim_set_keymap("n", "<C-k>", ":Autoformat<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-w>", ":ChooseWin<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F10>", ":Buffers<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F11>", ":Files<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F5>", ":NERDTreeToggle<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F6>", ":TagbarToggle<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<F9>", ":Ag<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<esc><esc>", ":noh<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<space>", "za", { noremap = true })
-vim.api.nvim_set_keymap("v", "<space>", "zf", { noremap = true })
 vim.b.SimpylFold_fold_docstring = 0
 vim.b.SimpylFold_fold_import = 0
 vim.cmd("hi clear SignColumn") -- https://news.ycombinator.com/item?id=5326397
 vim.cmd("let &sbr = nr2char(8618).' '") -- Show -> at the beginning of wrapped lines.
 vim.cmd("nnoremap c :bp\\|bd #<CR>")
-vim.cmd.colorscheme("solarized8")
+vim.cmd("set guioptions=Ace")
 vim.g.NERDTreeMouseMode = 3 -- Single-click to open files.
 vim.g.SimpylFold_fold_docstring = 0
 vim.g.SimpylFold_fold_import = 0
@@ -72,7 +66,6 @@ vim.o.clipboard = "unnamedplus" -- Use the system clipboard by default
 vim.o.colorcolumn = "80,120"
 vim.o.completeopt = "menu"
 vim.o.encoding = "utf-8"
-vim.o.guioptions = "Ace"
 vim.o.hidden = true -- Allow switching buffers even if current buffer is dirty
 vim.o.hlsearch = true -- Highlight search results
 vim.o.ignorecase = true -- Ignore case in search
@@ -88,95 +81,88 @@ vim.o.smartcase = true -- Ignore case if pattern does not include an uppercase l
 vim.o.termguicolors = true
 vim.o.timeoutlen = 1000
 vim.o.ttimeoutlen = 0 -- Timeout length for consecutive mappings and key codes, respectively
-vim.o.ttymouse = "sgr" -- Needed for mouse to play well with tmux
 vim.o.updatetime = 300 -- Length of time after typing stops to trigger plugin
 
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    -- https://github.com/preservim/nerdtree/issues/21
-    command = "if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | q | endif",
-})
+vim.cmd([[
+autocmd BufRead,BufNewFile wscript setfiletype python
+autocmd InsertEnter * set cul  " Highlight the current line in insert mode.
+autocmd InsertLeave * set nocul
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+autocmd vimenter * NERDTree
+autocmd vimenter * wincmd p
+nnoremap <C-k> :Autoformat<CR>
+nnoremap <C-w> :ChooseWin<CR>
+nnoremap <F10> :Buffers<CR>
+nnoremap <F11> :Files<CR>
+nnoremap <F5> :NERDTreeToggle<CR>
+nnoremap <F6> :TagbarToggle<CR>
+nnoremap <F9> :Ag<CR>
+nnoremap <esc><esc> :noh<return>
+nnoremap <space> za
+nnoremap c :bp\|bd #<CR>
+vnoremap <space> zf
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    callback = function(ev)
-        vim.o.ft = "python"
-    end,
-    pattern = { "wscript" },
-})
+" https://jdhao.github.io/2020/05/22/highlight_yank_region_nvim
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=700})
+augroup END
 
--- Highlight the current line in insert mode
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-    callback = function(ev)
-        vim.o.cul = true
-    end,
-})
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-    callback = function(ev)
-        vim.o.cul = false
-    end,
-})
+" https://stsievert.com/blog/2016/01/06/vim-jekyll-mathjax
+function! MathAndLiquid()
+    syn region math start=/\$\$/ end=/\$\$/
+    syn match math_block '\$[^$].\{-}\$'
+    syn match liquid '{%.*%}'
+    syn region highlight_block start='{% highlight .*%}' end='{%.*%}'
+    syn region highlight_block start='```' end='```'
+    hi link math Statement
+    hi link liquid Statement
+    hi link highlight_block Function
+    hi link math_block Function
+endfunction
+autocmd BufRead,BufNewFile,BufEnter *.md,*.markdown call MathAndLiquid()
+]])
 
--- https://alpha2phi.medium.com/neovim-for-beginners-lua-autocmd-and-keymap-functions-3bdfe0bebe42
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function(ev)
-        vim.highlight.on_yank()
-    end,
-})
-
--- https://stsievert.com/blog/2016/01/06/vim-jekyll-mathjax
-vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile", "BufRead" }, {
-    callback = function(ev)
-        vim.cmd([[
-        syn region math start=/\$\$/ end=/\$\$/
-        syn match math_block '\$[^$].\{-}\$'
-        syn match liquid '{%.*%}'
-        syn region highlight_block start='{% highlight .*%}' end='{%.*%}'
-        syn region highlight_block start='```' end='```'
-        hi link math Statement
-        hi link liquid Statement
-        hi link highlight_block Function
-        hi link math_block Function
-        ]])
-    end,
-    pattern = { "*.md", "*.markdown" },
-})
-
--- https://github.com/neovim/nvim-lspconfig
-local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({})
-lspconfig.clangd.setup({})
-require "lsp_signature".setup()
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
--- Use LspAttach autocommand to only map the following keys
+-- https://github.com/neovim/nvim-lspconfig/tree/v0.1.0
+local nvim_lsp = require('lspconfig')
+-- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set("n", "<space>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<space>f", function()
-            vim.lsp.buf.format({ async = true })
-        end, opts)
-    end,
-})
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'pyright' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+require "lsp_signature".setup()

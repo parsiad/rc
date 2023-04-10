7f9bba4cffd3c6f59df3ceb18181fad758ae53bc
Par Azimzadeh <parsiad@pdtpartners.com>
2023-04-10 10:54:08 -0400

    Change Magma keybinding

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -136,12 +136,12 @@ endfunction
 autocmd BufRead,BufNewFile,BufEnter *.md,*.markdown call MathAndLiquid()
 
 " https://github.com/dccsillag/magma-nvim
-nnoremap <silent><expr> <LocalLeader>r  :MagmaEvaluateOperator<CR>
-nnoremap <silent>       <LocalLeader>rr :MagmaEvaluateLine<CR>
-xnoremap <silent>       <LocalLeader>r  :<C-u>MagmaEvaluateVisual<CR>
-nnoremap <silent>       <LocalLeader>rc :MagmaReevaluateCell<CR>
-nnoremap <silent>       <LocalLeader>rd :MagmaDelete<CR>
-nnoremap <silent>       <LocalLeader>ro :MagmaShowOutput<CR>
+nnoremap <silent><expr> <LocalLeader>m  :MagmaEvaluateOperator<CR>
+nnoremap <silent>       <LocalLeader>mr :MagmaEvaluateLine<CR>
+xnoremap <silent>       <LocalLeader>m  :<C-u>MagmaEvaluateVisual<CR>
+nnoremap <silent>       <LocalLeader>mc :MagmaReevaluateCell<CR>
+nnoremap <silent>       <LocalLeader>md :MagmaDelete<CR>
+nnoremap <silent>       <LocalLeader>mo :MagmaShowOutput<CR>
 let g:magma_image_provider = "ueberzug"
 
 " https://gist.github.com/BlueDrink9/f40b3c816e5bcb349adcbc22eb753518

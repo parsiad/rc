2a67d43fcd49d30c6d48106ef2967e57b4b1c8a2
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2023-11-05 12:38:16 -0500

    Make prompt fail gracefully

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,9 @@
 setopt PROMPT_SUBST
-PROMPT='%F{4}[%F{3}$(hostname -s)%F{4}] %F{2}$(sed "s:\([^/]\)[^/]*/:\1/:g" <<<$PWD)%F{4}> %f'
+
+if command -v hostname &> /dev/null; then
+    HOSTNAME=$(hostname -s)
+else
+    HOSTNAME="?"
+fi
+
+PROMPT='%F{4}[%F{3}$HOSTNAME%F{4}] %F{2}$(sed "s:\([^/]\)[^/]*/:\1/:g" <<<$PWD)%F{4}> %f'

b597a801bbe7e4e187814a9bc33ff6508ec7469c
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-01-05 09:05:07 -0500

    Disable LuaSnip key bindings

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,10 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
-  { "folke/flash.nvim", enabled = false }
+  { "folke/flash.nvim", enabled = false },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
 }

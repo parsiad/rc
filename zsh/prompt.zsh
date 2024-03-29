f9500f04abbf1515c6b7044910a24130bf49e6d9
Par Azimzadeh <parsiad@pdtpartners.com>
2024-03-29 09:31:41 -0400

    Update zsh prompt to show branch name in Git repo

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,4 +6,14 @@ else
     HOSTNAME="?"
 fi
 
-PROMPT='%F{4}[%F{3}$HOSTNAME%F{4}] %F{2}$(sed "s:\([^/]\)[^/]*/:\1/:g" <<<$PWD)%F{4}> %f'
+git_super_status() {
+    local STATUS
+    STATUS=$(git rev-parse --is-inside-work-tree 2> /dev/null)
+    if [[ "$STATUS" == true ]]; then
+        local BRANCH
+        BRANCH=$(git symbolic-ref --short HEAD 2> /dev/null || git describe --tags --exact-match 2> /dev/null)
+        echo ' %F{5}'$BRANCH
+    fi
+}
+
+PROMPT='%F{3}$HOSTNAME%F{4} %F{2}$(sed "s:\([^/]\)[^/]*/:\1/:g" <<<$PWD)$(git_super_status)%F{4}> %f'

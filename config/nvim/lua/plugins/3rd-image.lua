ce6e1df1013579a797057e8cc4ea928887c5e470
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-07-08 19:04:44 -0400

    Use local Lua rocks path

--- config/nvim/lua/plugins/3rd-image.lua
+++ config/nvim/lua/plugins/3rd-image.lua
@@ -9,6 +9,10 @@ if not is_ueberzug_installed() then
   return {}
 end
 
+-- Example for configuring Neovim to load user-installed installed Lua rocks:
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
+
 return {
   "3rd/image.nvim",
   config = function()

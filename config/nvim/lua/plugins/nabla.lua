09ae4a0cae47040a35f658d422633db8e10e921d
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2024-11-25 21:44:42 -0500

    Update nabla.nvim configuration

--- config/nvim/lua/plugins/nabla.lua
+++ config/nvim/lua/plugins/nabla.lua
@@ -1,11 +1,23 @@
 -- take your scientific notes ✏️ in Neovim
 
+-- Requires LaTeX parser: install with `:TSInstall latex`.
+
 return {
   "jbyuki/nabla.nvim",
   event = "VeryLazy",
-  config = function()
-    if vim.bo.filetype == "markdown" then
-      require("nabla").enable_virt({ autogen = true })
-    end
+  -- https://github.com/jbyuki/nabla.nvim/issues/91
+  -- config = function()
+  --   if vim.bo.filetype == "markdown" then
+  --     require("nabla").enable_virt({ autogen = true })
+  --   end
+  -- end,
+  keys = function()
+    return {
+      {
+        "<leader>p",
+        ':lua require("nabla").popup()<cr>',
+        desc = "NablaPopUp",
+      },
+    }
   end,
 }

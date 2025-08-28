e57178617f6bb968a8917a29c1951fbc3f25fbfc
Parsiad Azimzadeh <parsiad.azimzadeh@gmail.com>
2025-08-27 21:06:08 -0400

    Update lock to use Dracula theme

---
+++ bin/i3lock.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Modified from https://github.com/dracula/i3lock-color
+
+alpha='dd'
+background='#282a36'
+selection='#44475a'
+comment='#6272a4'
+
+yellow='#f1fa8c'
+orange='#ffb86c'
+red='#ff5555'
+magenta='#ff79c6'
+blue='#6272a4'
+cyan='#8be9fd'
+green='50fa7b'
+
+i3lock \
+  --insidever-color=$selection$alpha \
+  --insidewrong-color=$selection$alpha \
+  --inside-color=$selection$alpha \
+  --ringver-color=$green$alpha \
+  --ringwrong-color=$red$alpha \
+  --ringver-color=$green$alpha \
+  --ringwrong-color=$red$alpha \
+  --ring-color=$blue$alpha \
+  --line-uses-ring \
+  --keyhl-color=$magenta$alpha \
+  --bshl-color=$orange$alpha \
+  --separator-color=$selection$alpha \
+  --verif-color=$green \
+  --wrong-color=$red \
+  --modif-color=$red \
+  --layout-color=$blue \
+  --date-color=$blue \
+  --time-color=$blue \
+  --blur 10 \
+  --clock \
+  --indicator \
+  --time-str="%H:%M:%S" \
+  --date-str="%a %b %e %Y" \
+  --verif-text="Verifying..." \
+  --wrong-text="Auth Failed" \
+  --noinput="No Input" \
+  --lock-text="Locking..." \
+  --lockfailed="Lock Failed" \
+  --radius=100 \
+  --ring-width=20 \
+  --pass-media-keys \
+  --pass-screen-keys \
+  --pass-volume-keys
